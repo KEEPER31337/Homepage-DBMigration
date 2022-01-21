41245ce50411bcef1b44381e6c44af22af0e01a4
shasuri <ghimmk@gmail.com>
2022-01-22 05:19:38 +0900

    Feat : my_database.py 추가
    pymysql을 편리하게 다루기 위한 class추가
    
    Footer : #6

---
+++ my_database.py
@@ -0,0 +1,31 @@
+from pymysql import connect, cursors
+
+from seperator.job_seperator import JobSeperator
+
+class MyDatabase :
+    db = ""
+    user = 'root'
+    passwd = ''
+    host = '127.0.0.1'
+    dbName = ''
+    charset='utf8'
+
+    jobSperator = ""
+    
+    def setUser(self,user) : self.user = user
+    def setDBName(self,dbName) : self.dbName = dbName
+    def setPasswd(self,passwd) : self.passwd = passwd
+
+    def setDB(self) :
+        self.db = connect(user=self.user,
+        passwd=self.passwd,
+        host=self.host,
+        db=self.dbName,
+        charset=self.charset
+    )
+
+    def getDB(self) : return self.db
+
+    def getCursor(self) :
+        return self.db.cursor(cursors.DictCursor)
+
