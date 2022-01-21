81dbb63b5bc0fa552d56d9101a7decf4e098fd13
shasuri <ghimmk@gmail.com>
2022-01-22 05:18:29 +0900

    Feat : migrator.py 추가
    전체 이관을 담당할 migrator.py 추가. 현재는 job_seperator.py만 적용중.
    
    Footer : #6

---
+++ migrator.py
@@ -0,0 +1,29 @@
+from my_database import MyDatabase
+from seperator.job_seperator import JobSeperator
+
+
+if __name__ == "__main__":
+    oldDB = MyDatabase()
+    oldDB.setDBName("keeper")
+    oldDB.setDB()
+
+    newDB = MyDatabase()
+    newDB.setDBName("keeper_new")
+    newDB.setDB()
+
+    jobSeperator = JobSeperator()
+
+    jobSeperator.addJobSrl("회장",34603,1)
+    jobSeperator.addJobSrl("부회장",53180,2)
+    jobSeperator.addJobSrl("대외부장",34131,3)
+    jobSeperator.addJobSrl("학술부장",34599,4)
+    jobSeperator.addJobSrl("전산관리자",34600,5)
+    jobSeperator.addJobSrl("서기",34601,6)
+    jobSeperator.addJobSrl("총무",34602,7)
+    jobSeperator.addJobSrl("사서",75521,8)
+
+    jobSeperator.setJobMemberTable(oldDB)
+    jobSeperator.insertJobMemberTable(newDB)
+    
+
+
