3debc079706fee27c65104c18ebf1cb2d5eb0b0f
shasuri <ghimmk@gmail.com>
2022-01-23 16:09:58 +0900

    Refactor : Method name changes
    src/migrator.py

--- src/migrator.py
+++ src/migrator.py
@@ -13,17 +13,17 @@ if __name__ == "__main__":
 
     jobSeperator = JobSeperator()
 
-    jobSeperator.addJobSrl("회장",34603,1)
-    jobSeperator.addJobSrl("부회장",53180,2)
-    jobSeperator.addJobSrl("대외부장",34131,3)
-    jobSeperator.addJobSrl("학술부장",34599,4)
-    jobSeperator.addJobSrl("전산관리자",34600,5)
-    jobSeperator.addJobSrl("서기",34601,6)
-    jobSeperator.addJobSrl("총무",34602,7)
-    jobSeperator.addJobSrl("사서",75521,8)
+    jobSeperator.addGroupSrl("회장",34603,1)
+    jobSeperator.addGroupSrl("부회장",53180,2)
+    jobSeperator.addGroupSrl("대외부장",34131,3)
+    jobSeperator.addGroupSrl("학술부장",34599,4)
+    jobSeperator.addGroupSrl("전산관리자",34600,5)
+    jobSeperator.addGroupSrl("서기",34601,6)
+    jobSeperator.addGroupSrl("총무",34602,7)
+    jobSeperator.addGroupSrl("사서",75521,8)
 
-    jobSeperator.setJobMemberTable(oldDB)
-    jobSeperator.insertJobMemberTable(newDB)
+    jobSeperator.selectGroupTable(oldDB)
+    jobSeperator.insertJobTable(newDB)
     
 
 
