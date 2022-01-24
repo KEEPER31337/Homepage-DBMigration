f401bda2a1aaa1ffd8277e1ae97098bc45a08bd1
shasuri <ghimmk@gmail.com>
2022-01-25 00:56:09 +0900

    Refactor : member_info_inserter.py의 클래스화 ExtraVarsInserter
    extra_vars_inserter.py
    extra_vars_parser.py
    *test 필요
    
    Footer : #3

---
+++ src/extra_vars_extractor/extra_vars_inserter.py
@@ -0,0 +1,46 @@
+
+# Add student number columns, parse extra_vars column and update rows.
+
+from db_controller.db_controller import DBController
+from extra_vars_extractor.extra_vars_parser import ExtraVarsParser
+
+
+class ExtraVarsInserter:
+    addStudentNumberColumnQuery = "ALTER TABLE xe_member ADD student_number VARCHAR(45) DEFAULT NULL"
+    selectMemberQuery = "SELECT member_srl, extra_vars FROM `xe_member`;"
+    updateMemberQuery = "UPDATE xe_member SET student_number = %(student_number)s WHERE member_srl = %(member_srl)s;"
+
+    dbController: DBController
+    memberRows: list
+
+    def setDBController(self, dbController: DBController) -> None:
+        self.dbController = dbController
+
+    def addColumns(self) -> None:
+        self.dbController.getCursor().execute(self.addStudentNumberColumnQuery)
+
+    def selectMemberRows(self) -> list:
+        cursor = self.dbController.getCursor()
+        cursor.execute(self.selectMemberQuery)
+        self.memberRows = cursor.fetchall()
+        return self.memberRows
+
+    def appendParsedExtraVars(self) -> list:
+        for i, row in enumerate(self.memberRows):
+            print(f"Member serial : {row['member_srl']}")
+            parsedExtraVars = ExtraVarsParser.parseExtraVars(row['extra_vars'])
+            print()
+            self.memberRows[i].update(parsedExtraVars)
+
+        return self.memberRows
+
+    def updateMemberRows(self) -> None:
+        self.dbController.getCursor().executemany(
+            self.updateMemberQuery, self.memberRows)
+        self.dbController.getDB().commit()
+
+    def insertExtraVars(self):
+        self.addColumns()
+        self.selectMemberRows()
+        self.appendParsedExtraVars()
+        self.updateMemberRows()
