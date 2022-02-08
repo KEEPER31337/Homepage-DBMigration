2b8a9a81bf8e06ff06fd000061ce395d4ec80460
shasuri <ghimmk@gmail.com>
2022-02-09 03:26:17 +0900

    Feat : 일반 데이터 이관 sql 코드 작성
    test/migrator_test.sql
    갈길이 멀다...
    
    Footer : #8

--- test/migrator_test.sql
+++ test/migrator_test.sql
@@ -1,16 +1,15 @@
 INSERT INTO
-    `member` (
+    `member`(
         id,
         login_id,
         email_address,
         password,
         real_name,
         nick_name,
-        birth_day,
-        register_date
-    )
+        birthday, 
+        student_id,
+        register_date)
 SELECT
-    (
         member_srl,
         user_id,
         email_address,
@@ -18,8 +17,150 @@ SELECT
         user_name,
         nick_name,
         birthday,
-        extravars,
+        member_srl,
         regdate
+FROM keeper.xe_member;
+
+INSERT INTO
+    `posting` (
+        id,
+        title,
+        /* clean_content, */
+        member_id,
+        visit_count,
+        like_count,
+        dislike_count,
+        comment_count,
+        register_time,
+        update_time,
+        ip_address,
+        allow_comment,
+        is_notice,
+        is_secret,
+        is_temp,
+        /* password, */
+        category_id
     )
-FROM
-    `xe_member`;
+SELECT
+    (
+        document_srl,
+        title,
+        /* content, */
+        member_srl,
+        readed_count,
+        voted_count,
+        blamed_count,
+        comment_count,
+        regdate,
+        last_update,
+        ipaddress,
+        comment_status,
+        is_notice,
+        status,
+        status,
+        /* password, */
+        module_srl
+    )
+FROM `keeper.xe_documents`;
+
+INSERT INTO
+    `comment` (
+        id,
+        /* clean_content, */
+        register_time,
+        update_time,
+        ip_address,
+        like_count,
+        dislike_count,
+        parent_id,
+        member_id,
+        posting_id
+    )
+SELECT
+    (
+        comment_srl,
+        /* content, */
+        regdate,
+        last_update,
+        ipaddress,
+        voted_count,
+        blamed_count,
+        parent_srl,
+        member_srl,
+        document_srl
+    )
+FROM `keeper.xe_comments`;
+
+INSERT INTO
+    `file` (
+        id,
+        file_name,
+        file_path,
+        file_size,
+        upload_time,
+        ip_address,
+        posting_id
+    )
+SELECT
+    (
+        file_srl,
+        source_filename,
+        uploaded_filename,
+        file_size,
+        regdate,
+        ipaddress,
+        source_filename
+    )
+FROM `keeper.xe_files`;
+
+INSERT INTO
+    `category` (
+        id,
+        name,
+        parent_id
+    )
+SELECT
+    (
+        module_srl,
+        browser_title,
+        parent_srl
+    )
+FROM `keeper.xe_modules`;
+
+INSERT INTO
+    `books` (
+        id,
+        title,
+        author
+    )
+SELECT
+    (
+        number,
+        name,
+        author
+    )
+FROM `Library.books`;
+
+INSERT INTO
+    `attendance`(
+        id,
+        time,
+        member_id,
+        point,
+        random_point,
+        ip_address,
+        greetings,
+        continous_day
+    )
+SELECT
+    (
+        attendance_srl,
+        regdate,
+        member_srl,
+        today_point,
+        random_point,
+        ipaddress,
+        greetings,
+        a_continuity
+    )
+FROM `keeper.attendance`;
