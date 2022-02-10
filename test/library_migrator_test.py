32a1404823e58bae1924eee78f4a41898f80c465
shasuri <ghimmk@gmail.com>
2022-02-10 19:24:33 +0900

    Test : book 이관 코드 테스트 작성 중
    new file:   test/library_migrator_test.py
    
    Footer : #12

---
+++ test/library_migrator_test.py
@@ -0,0 +1,11 @@
+
+from library_migrator.book_migrator import BookMigrator
+
+
+bookMigrator = BookMigrator()
+bookMigrator.addBookDepartment(0, 1)
+bookMigrator.addBookDepartment(1, 2)
+bookMigrator.addBookDepartment(2, 3)
+bookMigrator.addBookDepartment(3, 4)
+bookMigrator.addBookDepartment(4, 5)
+bookMigrator.addBookDepartment(9, 6)
