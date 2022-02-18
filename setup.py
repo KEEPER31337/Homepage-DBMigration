e649a3b2514d17990b73ea54c36dd867a6bfdd3a
shasuri <ghimmk@gmail.com>
2022-02-18 20:07:54 +0900

    Feat : 전체 이관 script 작성 및 src 디렉토리명 변경
    renamed:    src/__init__.py -> db_migration/__init__.py
    renamed:    src/__main__.py -> db_migration/__main__.py
    new file:   script/keeper_copy_setter.sql
    new file:   script/migrate_keeper_db.sh
    modified:   setup.py
    
    Footer : #8

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name='db_migration',
     version='0.1',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src')
+    package_dir={'': 'db_migration'},
+    packages=find_packages(where='db_migration')
 )
