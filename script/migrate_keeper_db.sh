186bed672ce6ceab1c1e5fcefb3c9ddfa9069696
shasuri <ghimmk@gmail.com>
2022-02-21 22:55:59 +0900

    Chore : 이관 준비
    modified:   README.md
    modified:   db_migration/db_controller/db_controller.py
    modified:   script/migrate_keeper_db.sh

--- script/migrate_keeper_db.sh
+++ script/migrate_keeper_db.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 pip install -r requirements.txt
+
+# 구 DB명 바꿔서 dump 적용
+# copy DB set
 mysql -u root -p < resource/keeper_copy_setter.sql
-# mysql -u root -p < resource/init.sql
-# Remove category default values
+
+# 신 DB init.sql의 category 기본값 삭제
+# 신 DB init.sql을 resource/로 옮기기
+mysql -u root -p < resource/init.sql
+
+# db_migration/__main__.py에서 copy DB명, 신 DB명 맞추기
 python db_migration
-# Category matching
+
 # Duplicated equipment merging
+# 구 DB 및 copy DB삭제
