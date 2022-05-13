72019ab44d27a4fa06128cc2eb2511b187dc7fb9
shasuri <ghimmk@gmail.com>
2022-05-13 20:16:43 +0900

    Feat : type 추가

--- db_migration/util/typedef.py
+++ db_migration/util/typedef.py
@@ -3,5 +3,5 @@ from typing import Dict, List, Tuple, Union
 Row = Dict[str, Union[int, str]]
 Table = List[Row]
 
-CategoryIdPair = Tuple[int,int]
+CategoryIdPair = Tuple[int, int]
 CategoryInfo = Tuple[int, str, int, str]
